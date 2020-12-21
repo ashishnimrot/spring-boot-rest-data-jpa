e25a14b1b4842848f467033993e87bd9b2b5a469
Ashish Nimrot <ashishnimrot@gmail.com>
2020-12-21 18:29:19 +0530

    first commit

---
+++ src/main/java/com/alten/springbootrestdatajpa/SprinBootRestDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.alten.springbootrestdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootRestDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootRestDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alten/springbootrestdatajpa/SprinBootRestDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.springbootrestdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootRestDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
