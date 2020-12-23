922ef2ab44b85ff2260ae1a580f7b89527a462ba
Ashish Nimrot <ashishnimrot@gmail.com>
2020-12-23 20:22:44 +0530

    Add Validation

--- src/main/java/com/alten/springbootrestdatajpa/controller/EmployeeController.java
+++ src/main/java/com/alten/springbootrestdatajpa/controller/EmployeeController.java
@@ -6,14 +6,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alten.springbootrestdatajpa.model.Employee;
 import com.alten.springbootrestdatajpa.repository.EmployeeRepository;
@@ -44,6 +51,29 @@ public class EmployeeController {
 		}
 		return new ResponseEntity<Employee>(new Employee(), HttpStatus.NOT_FOUND);
 	}
+	
+	@PostMapping
+	public ResponseEntity<?> createEmployee(@Valid @RequestBody Employee entity,UriComponentsBuilder uriComponentsBuilder,HttpServletRequest request) {
+	
+		Employee newEmployee = employeeRepository.save(entity);
+		UriComponents uriComponents = uriComponentsBuilder
+				.path(request.getRequestURI()+"/{id}")
+				.buildAndExpand(newEmployee.getEmpId());
+		
+		
+	  return ResponseEntity.created(uriComponents.toUri()).body(newEmployee);
+	}
+	
+//	@PutMapping("/{id}")
+//	public ResponseEntity<Employee> updateProduct(@PathVariable("id") Integer id,
+//			@Valid @RequestBody Product product ) throws ResourceNotFoundException {
+//		Employee product2 = productService.getProductById(id)
+//				.orElseThrow(()-> new ResourceNotFoundException("Product not found"));
+//		product.setProductId(id);
+//		Employee product3 =productService.createProduct(product);
+//		
+//		return ResponseEntity.ok(product3);
+//	}
 	
 	@DeleteMapping("{id}")
 	public ResponseEntity<Map<String, Boolean>> removeEmployeeById(@PathVariable int id) {
--- src/main/java/com/alten/springbootrestdatajpa/model/Employee.java
+++ src/main/java/com/alten/springbootrestdatajpa/model/Employee.java
@@ -1,10 +1,15 @@
 package com.alten.springbootrestdatajpa.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,8 +23,18 @@ import lombok.NoArgsConstructor;
 public class Employee {
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)	
 	private int empId;
+	
+	@NotNull(message = "Firstname should not be blank!")
 	private String firstName;
+	
+	@NotNull(message = "Lastname should not be blank!")
 	private String lastName;
-	private float salary;
+	
+	@NotNull(message = "Salary should not be blank!")
+	@DecimalMin(value = "1.0", message = "provide till 2 decimal places")
+	@DecimalMax(value = "100000.0", message = "provide till 9 decimal places")
+	private BigDecimal salary;
+	
+	@NotNull(message = "Dept Id should not be blank!")
 	private int deptId;
 }
